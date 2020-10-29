764bafccaf5ead8492c1b13b6265a5ac997d9ec0
Hulusi Bumin Yalçın <70579311+HBuminY@users.noreply.github.com>
2020-10-29 19:49:18 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,5 @@
+import "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+
+$.getJSON('https://json.geoiplookup.io/api?callback=?', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
