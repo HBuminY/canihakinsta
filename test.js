a85a75f899a9018d2dd8d1080618984eaa1470a3
Hulusi Bumin Yalçın <70579311+HBuminY@users.noreply.github.com>
2020-12-10 13:36:20 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-import jQuery from "jquery";
-
-jQuery.getJSON('https://json.geoiplookup.io/api?callback=?', function(data) {
-  jQuery.post("https://hby-io.glitch.me/lol",data,()=>{});
-});
+let i;
+for(i=1; i<100; i++){
+    if(Number.isInteger(i/15)){console.log('fizz buzz');}
+    else if(Number.isInteger(i/3)){console.log('fizz');}
+    else if(Number.isInteger(i/5)){console.log('buzz');}
+    
+    else{console.log(i);}
+}
